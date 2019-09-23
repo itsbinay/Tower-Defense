87560b268b66ace88866904d08c41b7c926b7d7b
Binay Gurung <binaygurung9@gmail.com>
2019-09-23 11:25:49 +0800

    Okay some changes here and there

--- src/main/java/sample/MyController.java
+++ src/main/java/sample/MyController.java
@@ -10,7 +10,7 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.geometry.Insets;
-import javafx.scene.paint.Color;
+import javafx.scene.paint.Color; 
 
 public class MyController {
     @FXML

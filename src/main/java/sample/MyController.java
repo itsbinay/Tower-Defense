9de5501b27895f72a0221ef430ff6137e58a2f72
Aayush <apple@Aayushs-MacBook-Pro.local>
2019-11-08 18:35:00 +0800

    move aayush

--- src/main/java/sample/MyController.java
+++ src/main/java/sample/MyController.java
@@ -20,37 +20,37 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class MyController {
-<<<<<<< HEAD
-	@FXML
-	private Button buttonNextFrame;
 
-	@FXML
-	private Button buttonSimulate;
+    @FXML
+    private Button buttonNextFrame;
 
-	@FXML
-	private Button buttonPlay;
+    @FXML
+    private Button buttonSimulate;
 
-	@FXML
-	private AnchorPane paneArena;
+    @FXML
+    private Button buttonPlay;
 
-	@FXML
-	private Label labelBasicTower;
+    @FXML
+    private AnchorPane paneArena;
 
-	@FXML
-	private Label labelIceTower;
+    @FXML
+    private Label labelBasicTower;
 
-	@FXML
-	private Label labelCatapult;
+    @FXML
+    private Label labelIceTower;
 
-	@FXML
-	private Label labelLaserTower;
+    @FXML
+    private Label labelCatapult;
 
-	private static final int ARENA_WIDTH = 480;
-	private static final int ARENA_HEIGHT = 480;
-	private static final int GRID_WIDTH = 40;
-	private static final int GRID_HEIGHT = 40;
-	private static final int MAX_H_NUM_GRID = 12;
-	private static final int MAX_V_NUM_GRID = 12;
+    @FXML
+    private Label labelLaserTower;
+
+    private static final int ARENA_WIDTH = 480;
+    private static final int ARENA_HEIGHT = 480;
+    private static final int GRID_WIDTH = 40;
+    private static final int GRID_HEIGHT = 40;
+    private static final int MAX_H_NUM_GRID = 12;
+    private static final int MAX_V_NUM_GRID = 12;
 	private static int monsterCounter = 0;
 	private static int nextFrameCounter = 0;
 	private static int[] startCoord = { 0, 0 };
@@ -60,60 +60,58 @@ public class MyController {
 
 	ArrayList<Monster> monsterList = new ArrayList<Monster>();
 
-	private Label grids[][] = new Label[MAX_V_NUM_GRID][MAX_H_NUM_GRID]; // the grids on arena
-	private int x = -1, y = 0; // where is my monster
+    private Label grids[][] = new Label[MAX_V_NUM_GRID][MAX_H_NUM_GRID]; //the grids on arena
 
-	/**
-	 * A dummy function to show how button click works
-	 */
-	@FXML
-	private void play() {
-		System.out.println("Play button clicked");
+    /**
+     * A dummy function to show how button click works
+     */
+    @FXML
+    private void play() {
+        System.out.println("Play button clicked");
 
-		Label newLabel = new Label();
-		newLabel.setLayoutX(GRID_WIDTH / 4);
-		newLabel.setLayoutY(GRID_WIDTH / 4);
-		newLabel.setMinWidth(GRID_WIDTH / 2);
-		newLabel.setMaxWidth(GRID_WIDTH / 2);
-		newLabel.setMinHeight(GRID_WIDTH / 2);
-		newLabel.setMaxHeight(GRID_WIDTH / 2);
-		newLabel.setStyle("-fx-border-color: black;");
-		newLabel.setText("*");
-		newLabel.setBackground(new Background(new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, Insets.EMPTY)));
-		paneArena.getChildren().addAll(newLabel);
-	}
+        Label newLabel = new Label();
+        newLabel.setLayoutX(GRID_WIDTH / 4);
+        newLabel.setLayoutY(GRID_WIDTH / 4);
+        newLabel.setMinWidth(GRID_WIDTH / 2);
+        newLabel.setMaxWidth(GRID_WIDTH / 2);
+        newLabel.setMinHeight(GRID_WIDTH / 2);
+        newLabel.setMaxHeight(GRID_WIDTH / 2);
+        newLabel.setStyle("-fx-border-color: black;");
 
-	/**
-	 * A function that create the Arena
-	 */
-	@FXML
-	public void createArena() {
-		if (grids[0][0] != null)
-			return; // created already
-		for (int i = 0; i < MAX_V_NUM_GRID; i++)
-			for (int j = 0; j < MAX_H_NUM_GRID; j++) {
-				Label newLabel = new Label();
-				if (j % 2 == 0 || i == ((j + 1) / 2 % 2) * (MAX_V_NUM_GRID - 1))
-					newLabel.setBackground(
-							new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-				else
-					newLabel.setBackground(
-							new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
-				newLabel.setLayoutX(j * GRID_WIDTH);
-				newLabel.setLayoutY(i * GRID_HEIGHT);
-				newLabel.setMinWidth(GRID_WIDTH);
-				newLabel.setMaxWidth(GRID_WIDTH);
-				newLabel.setMinHeight(GRID_HEIGHT);
-				newLabel.setMaxHeight(GRID_HEIGHT);
-				newLabel.setStyle("-fx-border-color: black;");
-				grids[i][j] = newLabel;
-				paneArena.getChildren().addAll(newLabel);
-			}
+        newLabel.setText("|-|");
+        newLabel.setBackground(new Background(new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, Insets.EMPTY)));
 
-		setDragAndDrop();
-	}
+        paneArena.getChildren().addAll(newLabel);
+    }
 
-	public void monsterGenerate() {
+    /**
+     * A function that create the Arena
+     */
+    @FXML
+    public void createArena() {
+        if (grids[0][0] != null)
+            return; //created already
+        for (int i = 0; i < MAX_V_NUM_GRID; i++)
+            for (int j = 0; j < MAX_H_NUM_GRID; j++) {
+                Label newLabel = new Label();
+                if (j % 2 == 0 || i == ((j + 1) / 2 % 2) * (MAX_V_NUM_GRID - 1))
+                    newLabel.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+                else
+                    newLabel.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+                newLabel.setLayoutX(j * GRID_WIDTH);
+                newLabel.setLayoutY(i * GRID_HEIGHT);
+                newLabel.setMinWidth(GRID_WIDTH);
+                newLabel.setMaxWidth(GRID_WIDTH);
+                newLabel.setMinHeight(GRID_HEIGHT);
+                newLabel.setMaxHeight(GRID_HEIGHT);
+                newLabel.setStyle("-fx-border-color: black;");
+                grids[i][j] = newLabel;
+                paneArena.getChildren().addAll(newLabel);
+            }
+
+        setDragAndDrop();
+    }
+    public void monsterGenerate() {
 		System.out.println("monsterGenerate");
 		int result = r.nextInt(high - low) + low;
 
@@ -187,16 +185,15 @@ public class MyController {
 
 	}
 
-	@FXML
-	private void nextFrame() {
-
+    @FXML
+    private void nextFrame() {
+    	
 		for (int i = 0; i < monsterList.size(); i++) {
 
 			boolean down = false;
 			boolean right = false;
 
 			System.out.println("RUNS");
-
 			int movementSpeed = monsterList.get(i).getMovementSpeed();
 			int x = monsterList.get(i).getX();
 			int y = monsterList.get(i).getY();
@@ -249,156 +246,6 @@ public class MyController {
 
 		nextFrameCounter++;
 		System.out.println("nextFrameCounter" + nextFrameCounter);
-	}
-
-	/**
-	 * A function that demo how drag and drop works
-	 */
-	private void setDragAndDrop() {
-		Label target = grids[3][3];
-		target.setText("Drop\nHere");
-		Label source1 = labelBasicTower;
-		Label source2 = labelIceTower;
-		source1.setOnDragDetected(new DragEventHandler(source1));
-		source2.setOnDragDetected(new DragEventHandler(source2));
-
-		target.setOnDragDropped(new DragDroppedEventHandler());
-
-		// well, you can also write anonymous class or even lambda
-		// Anonymous class
-		target.setOnDragOver(new EventHandler<DragEvent>() {
-			public void handle(DragEvent event) {
-				/* data is dragged over the target */
-				System.out.println("onDragOver");
-
-				/*
-				 * accept it only if it is not dragged from the same node and if it has a string
-				 * data
-				 */
-				if (event.getGestureSource() != target && event.getDragboard().hasString()) {
-					/* allow for both copying and moving, whatever user chooses */
-					event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-				}
-
-				event.consume();
-			}
-		});
-
-		target.setOnDragEntered(new EventHandler<DragEvent>() {
-			public void handle(DragEvent event) {
-				/* the drag-and-drop gesture entered the target */
-				System.out.println("onDragEntered");
-				/* show to the user that it is an actual gesture target */
-				if (event.getGestureSource() != target && event.getDragboard().hasString()) {
-					target.setStyle("-fx-border-color: blue;");
-				}
-
-				event.consume();
-			}
-		});
-		// lambda
-		target.setOnDragExited((event) -> {
-			/* mouse moved away, remove the graphical cues */
-			target.setStyle("-fx-border-color: black;");
-			System.out.println("Exit");
-			event.consume();
-		});
-	}
-=======
-    @FXML
-    private Button buttonNextFrame;
-
-    @FXML
-    private Button buttonSimulate;
-
-    @FXML
-    private Button buttonPlay;
-
-    @FXML
-    private AnchorPane paneArena;
-
-    @FXML
-    private Label labelBasicTower;
-
-    @FXML
-    private Label labelIceTower;
-
-    @FXML
-    private Label labelCatapult;
-
-    @FXML
-    private Label labelLaserTower;
-
-    private static final int ARENA_WIDTH = 480;
-    private static final int ARENA_HEIGHT = 480;
-    private static final int GRID_WIDTH = 40;
-    private static final int GRID_HEIGHT = 40;
-    private static final int MAX_H_NUM_GRID = 12;
-    private static final int MAX_V_NUM_GRID = 12;
-
-    private Label grids[][] = new Label[MAX_V_NUM_GRID][MAX_H_NUM_GRID]; //the grids on arena
-    private int x = -1, y = 0; //where is my monster
-    /**
-     * A dummy function to show how button click works
-     */
-    @FXML
-    private void play() {
-        System.out.println("Play button clicked");
-
-        Label newLabel = new Label();
-        newLabel.setLayoutX(GRID_WIDTH / 4);
-        newLabel.setLayoutY(GRID_WIDTH / 4);
-        newLabel.setMinWidth(GRID_WIDTH / 2);
-        newLabel.setMaxWidth(GRID_WIDTH / 2);
-        newLabel.setMinHeight(GRID_WIDTH / 2);
-        newLabel.setMaxHeight(GRID_WIDTH / 2);
-        newLabel.setStyle("-fx-border-color: black;");
-
-        newLabel.setText("|-|");
-        newLabel.setBackground(new Background(new BackgroundFill(Color.YELLOW, CornerRadii.EMPTY, Insets.EMPTY)));
-
-        paneArena.getChildren().addAll(newLabel);
-    }
-
-    /**
-     * A function that create the Arena
-     */
-    @FXML
-    public void createArena() {
-        if (grids[0][0] != null)
-            return; //created already
-        for (int i = 0; i < MAX_V_NUM_GRID; i++)
-            for (int j = 0; j < MAX_H_NUM_GRID; j++) {
-                Label newLabel = new Label();
-                if (j % 2 == 0 || i == ((j + 1) / 2 % 2) * (MAX_V_NUM_GRID - 1))
-                    newLabel.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
-                else
-                    newLabel.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
-                newLabel.setLayoutX(j * GRID_WIDTH);
-                newLabel.setLayoutY(i * GRID_HEIGHT);
-                newLabel.setMinWidth(GRID_WIDTH);
-                newLabel.setMaxWidth(GRID_WIDTH);
-                newLabel.setMinHeight(GRID_HEIGHT);
-                newLabel.setMaxHeight(GRID_HEIGHT);
-                newLabel.setStyle("-fx-border-color: black;");
-                grids[i][j] = newLabel;
-                paneArena.getChildren().addAll(newLabel);
-            }
-
-        setDragAndDrop();
-    }
-
-    @FXML
-    private void nextFrame() {
-        if (x == -1) {
-            grids[0][0].setText("M");
-            x = 0;
-            return;
-        }
-        if (y == MAX_V_NUM_GRID - 1)
-            return;
-        grids[y++][x].setText("");
-        grids[y][x].setText("M");
     }
 
     /**
@@ -454,7 +301,7 @@ public class MyController {
                 event.consume();
         });
     }
->>>>>>> 78d92e599460151bb45c26a781661676561d6b35
+
 }
 
 class DragEventHandler implements EventHandler<MouseEvent> {
